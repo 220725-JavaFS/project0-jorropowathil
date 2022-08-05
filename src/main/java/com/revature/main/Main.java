1abbc19046d61fdb5c3b5d6d5f9128696699e715
jorropowathil <jorispowathil@gmail.com>
2022-08-04 20:55:34 -0500

    Added scanner stuff

---
+++ src/main/java/com/revature/main/Main.java
@@ -0,0 +1,19 @@
+package com.revature.main;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		//New scanner variable start = to new scanner object that takes in system input
+		Scanner start = new Scanner(System.in);
+		System.out.print("--------------Welcome to the employee database system!--------------");
+		System.out.print("Please make your selection");
+		
+		System.out.print("1.) ");
+		System.out.print("2.) ");
+		System.out.print("3.) ");
+		
+	}
+
+}
