82e34e4cf382c4ad6ba0897440b907f3de32d411
jorropowathil <jorispowathil@gmail.com>
2022-08-04 21:05:11 -0500

    New line selection

--- src/main/java/com/revature/main/Main.java
+++ src/main/java/com/revature/main/Main.java
@@ -7,12 +7,15 @@ public class Main {
 	public static void main (String[] args) {
 		//New scanner variable start = to new scanner object that takes in system input
 		Scanner start = new Scanner(System.in);
-		System.out.print("--------------Welcome to the employee database system!--------------");
-		System.out.print("Please make your selection");
+		System.out.print("--------------Welcome to the employee database system!--------------\n");
+	
 		
-		System.out.print("1.) ");
-		System.out.print("2.) ");
-		System.out.print("3.) ");
+		
+		System.out.print("Please make your selection\n");
+		
+		System.out.print("1.) \n");
+		System.out.print("2.) \n");
+		System.out.print("3.) \n");
 		
 	}
 
