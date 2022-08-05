a5610e8b8643c259c9ac33d7470fb6a00a27011c
jorropowathil <jorispowathil@gmail.com>
2022-08-04 21:49:11 -0500

    more code some scans

--- src/main/java/com/revature/main/Main.java
+++ src/main/java/com/revature/main/Main.java
@@ -5,18 +5,39 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main (String[] args) {
+		System.out.print("----------------------------Welcome to the employee database system!----------------------------\n");
+		System.out.print("Please make your selection \n");
+		System.out.print("1.) Login \n");
+		System.out.print("2.) Sign Up\n");
+		
+		System.out.print("3.) Exit program \n");
+		
 		//New scanner variable start = to new scanner object that takes in system input
-		Scanner start = new Scanner(System.in);
-		System.out.print("--------------Welcome to the employee database system!--------------\n");
+		Scanner scan = new Scanner(System.in);
+		
+		int selection = scan.nextInt();
+		
+		System.out.println("You selected " + selection);
+		
+		switch(selection) {
+		  case 1:
+			  System.out.println("Please enter your Username");
+			  
+			  String userName = scan.nextLine();
+			  
+			  
+			  
+			  break;
+		  case 2:
+			  System.out.println("You selected " + selection);		    break;
+		  default:
+		    // code block
+		}
+
+		
 	
-		
-		
-		System.out.print("Please make your selection\n");
-		
-		System.out.print("1.) \n");
-		System.out.print("2.) \n");
-		System.out.print("3.) \n");
-		
+	
+	
 	}
 
 }
---
+++ src/main/java/com/revature/util/menuOptions.java
@@ -0,0 +1,16 @@
+package com.revature.util;
+
+
+public class menuOptions {
+	
+	public static void menuoptions (String[] args) {
+		
+		String menuHeader = "a";
+		System.out.print(menuHeader);
+		
+		
+		
+	}
+
+
+}
