da65cb2a72d1f736bbfe43a70aab4ddc01396b95
jorropowathil <jorispowathil@gmail.com>
2022-08-07 15:53:34 -0500

    Additional menu validation

--- src/main/java/com/revature/main/Main.java
+++ src/main/java/com/revature/main/Main.java
@@ -21,17 +21,34 @@ public class Main {
 		
 		switch(selection) {
 		  case 1:
-			  System.out.println("Please enter your Username");
+			  System.out.println("Please enter your username");
 			  
-			  String userName = scan.nextLine();
-			  
-			  
-			  
-			  break;
 		  case 2:
-			  System.out.println("You selected " + selection);		    break;
+			  System.out.println("Please pick your Username");
+			  String newEmployee = scan.next();
+			  System.out.println("Welcome back " + newEmployee + "\nPlease choose your password.");
+			  String newPassword = scan.next();
+			  System.out.println("Please confirm your password");
+			  String confirmPassword = scan.next();
+		
+			  
+			  if (newPassword.equals(confirmPassword.trim())) { 
+				  System.out.println("Your password is: " + confirmPassword);
+				  break;
+			  }
+			  else {
+				  System.out.println(newPassword + "<---- Password 1");
+				  System.out.println(confirmPassword+ "<---- Password 2");
+				  System.out.println("They do not match! \nExiting program...");
+				 
+			  }
+			  break;
+		  case 3:
+			  System.out.println("Exiting program! See you again soon!");
+			  break;
 		  default:
-		    // code block
+			
+		   
 		}
 
 		
